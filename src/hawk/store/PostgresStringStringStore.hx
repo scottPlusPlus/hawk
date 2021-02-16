bd25b03503bb5bf06cd663c1ee4c554a2943435c
scott <scott@scottplusplus.com>
2021-02-16 08:11:28 -0500

    update to PostGres WIP

--- src/hawk/store/PostgresStringStringStore.hx
+++ src/hawk/store/PostgresStringStringStore.hx
@@ -8,24 +8,25 @@ class PostgresStringStringStore implements IKVStore<String,String> {
     private var _postgresClient:Dynamic;
     private var _tableName:String;
 
-    public function new(postgresClient:Dynamic){
+    public function new(postgresClient:Dynamic, tableName){
         _postgresClient = postgresClient;
+        _tableName = tableName;
 
-        var query = '
-        CREATE TABLE ${_tableName} (
-            key varchar,
-            val varchar
-        );
-        ';
+        // var query = '
+        // CREATE TABLE ${_tableName} (
+        //     key varchar,
+        //     val varchar
+        // );
+        // ';
 
-        _postgresClient.query(query, function(err:Dynamic, res:Dynamic){
-            if (err != null) {
-                Log.error('error creating table ${_tableName} in postgres:  ${err}');
-                return;
-            }
-            Log.debug('successfully created table ${_tableName} in db');
-            _postgresClient.end();
-        });
+        // _postgresClient.query(query, function(err:Dynamic, res:Dynamic){
+        //     if (err != null) {
+        //         Log.error('error creating table ${_tableName} in postgres:  ${err}');
+        //         return;
+        //     }
+        //     Log.debug('successfully created table ${_tableName} in db');
+        //     _postgresClient.end();
+        // });
     }
 
 	public function exists(key:String):Promise<Bool>{
@@ -33,7 +34,20 @@ class PostgresStringStringStore implements IKVStore<String,String> {
     }
 
 	public function get(key:String):Promise<Null<String>>{
-        return null;
+        var query = 'SELECT * FROM ${_tableName} WHERE key = $1';
+        var p = new PromiseTrigger<String>();
+        _postgresClient.query(query, key, function(err:Dynamic, res:Dynamic) {
+            if (err != null) {
+                var e = new Error('error creating table ${_tableName} in postgres:  ${err}');
+                Log.error(e);
+                p.reject(e);
+            }
+            Log.debug('postgres get success for ${key}');
+            var str = Std.string(res.rows);
+            _postgresClient.end();
+            p.resolve(str);
+        });
+        return p;
     }
 
 	public function getSure(key:String):Promise<String>{
@@ -41,7 +55,7 @@ class PostgresStringStringStore implements IKVStore<String,String> {
     }
 
 	public function set(key:String, value:String):Promise<String>{
-        var query = 'INSERT INTO users (key, val) VALUES ("${key}", "${value}")';
+        var query = 'INSERT INTO ${_tableName} (key, val) VALUES ("${key}", "${value}")';
         var p = new PromiseTrigger<String>();
         _postgresClient.query(query,  function(err:Dynamic, res:Dynamic) {
             if (err != null) {
