4cd19c2d7a57ad128a9d8de6bad7860d06eb7350
scott <scott@scottplusplus.com>
2022-01-16 21:38:57 -0800

    adding WriteThroughLRUCache

---
+++ src/hawk/store/WriteThroughLRUCache.hx
@@ -0,0 +1,25 @@
+package hawk.store;
+
+import hawk.async_iterator.AsyncIterator;
+import tink.core.Promise;
+
+class WriteThroughLRUCache<V> extends LRUCache<V> implements IKVStore<String,V> {
+
+    private var _fullTruthStore:IKVStore<String,V>;
+
+    public function new(cacheStore:IKVStore<String, V>, truthStore:IKVStore<String, V>, capacity:UInt = 128) {
+        super(cacheStore, truthStore, capacity);
+        _fullTruthStore = truthStore;   
+    }
+
+	public override function set(key:String, value:V):Promise<V> {
+        return _fullTruthStore.set(key, value).next(function(res){
+            return _cacheStore.set(key, value);
+        });
+	}
+
+    public function keyValueIterator():AsyncIterator<KV<String,V>>{
+        return _fullTruthStore.keyValueIterator();
+    }
+
+}
